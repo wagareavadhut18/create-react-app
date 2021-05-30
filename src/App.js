2484712e18b5cefa9f0e09e7d744c42d7bf43d60
Neosoft <avadutvagare321@gmail.com>
2021-05-30 13:51:15 +0530

    Signup Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
+import Login from "./components/Signup";
 
 function App() {
   return (
     <div>
         <Navbar></Navbar>
         <Carousel></Carousel>
+        <Login></Login>
     </div>
   );
 }
