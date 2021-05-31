5ed4ac40ad61a6ae5d778624ed8b523214e772c2
Neosoft <avadutvagare321@gmail.com>
2021-05-31 15:21:16 +0530

    Display cake list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
-import Login from "./components/Signup";
+// import Login from "./components/Signup";
+import Cakelist from "./components/Cakelist";
 
 function App() {
   return (
     <div>
         <Navbar></Navbar>
         <Carousel></Carousel>
-        <Login></Login>
+        {/* <Login></Login> */}
+        <Cakelist></Cakelist>
     </div>
   );
 }
---
+++ src/components/Cake.js
@@ -0,0 +1,18 @@
+function Cake(props){
+    if(props.cake){
+            return (
+                <div className="card mt-3 mr-3" style={{width:"15rem"}}>
+                    {props.cake.image && <img className="card-img-top" src={props.cake.image} alt="Card image cap" style={{height:"200px"}}/> }
+                    <div className="card-body">
+                        <h5 className="card-title">{props.cake.name}</h5>
+                        <label className="card-text">&#8377;{props.cake.price}</label>
+                    </div>
+                </div>
+            )
+  
+    }
+    else{
+      return null
+    }
+  }
+  export default Cake
